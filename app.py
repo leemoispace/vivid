8cc82e51a3deca9a496ef865cd614223e70d4276
leemoispace <leemoispace@gmal.com>
2016-07-03 11:43:38 +0300

    !

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-        return 'vivid is beautiful,auto root restart'
+        return 'vivid is beautiful,auto root restart!'
 
 @app.route('/data')
 def name():
