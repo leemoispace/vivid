40c8e63b1422c7dfe2da43297d3aa37aee778618
leemoispace <leemoispace@gmal.com>
2016-07-03 16:12:08 +0300

    variable in url

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app=Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
-@app.route('/data')
-def name():
-        data={"names":["John","Jacob"]}
-        return jsonify(data)
+
+# url中的变量
+@app.route('/user/<name>')
+def user(name):
+        return '<h1>hello,%s!</h1>' %name
 
 if __name__=='__main__':
         app.run()
